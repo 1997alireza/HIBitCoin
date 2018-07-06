22e03328558b67eca1db80a411cc1c954dfd95ea
1997alireza <torabian.alireza@gmail.com>
2018-07-06 18:32:00 +0430

    fix sha.vhd

--- SHA.vhd
+++ SHA.vhd
@@ -1,46 +1,39 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
-ENTITY SimpleFSM is
-generic ( msg_length : integer := 4 );
-PORT (clock:    IN STD_LOGIC;
+entity SHA is
+generic (msg_length : integer := 4 );
+port (clock:    IN STD_LOGIC;
       P:        IN STD_LOGIC;
       reset:    IN STD_LOGIC;
       msg:      IN STD_LOGIC_VECTOR(msg_length-1 downto 0);
-      hash : out STD_LOGIC_VECTOR(255 downto 0)
-END SimpleFSM;
+      hash : out STD_LOGIC_VECTOR(255 downto 0))
+end entity;
 
-Architecture RTL of SimpleFSM is
-TYPE State_type IS (PADDING, BLOCK_PROCESS, HASH_PROCESS);  
-	SIGNAL State : State_Type;    
-							     
-BEGIN 
-  PROCESS (clock, reset) 
-  BEGIN 
-    If (reset = '1') THEN            
+architecture RTL of SHA is
+type STATE_TYPE is (PADDING, BLOCK_PROCESS, HASH_PROCESS);  
+	signal State : STATE_TYPE;				     
+begin 
+  process (clock, reset) 
+  begin 
+    if (reset = '1') then            
 	   State <= PADDING;
- 
-    ELSIF rising_edge(clock) THEN   
-        CASE State IS
+    elsif rising_edge(clock) then   
+        case State is
      
-            WHEN PADDING => 
-                IF P='1' THEN 
-                    State <= BLOCK_PROCESS; 
-                END IF; 
-     
-            WHEN BLOCK_PROCESS => 
-                IF P='1' THEN 
+            when PADDING => 
+                State <= BLOCK_PROCESS; 
+            when BLOCK_PROCESS => 
+                if P='1' then 
                     State <= HASH_PROCESS; 
-                END IF; 
-            WHEN HASH_PROCESS => 
-                IF P='1' THEN 
+                end if; 
+            when HASH_PROCESS => 
+                if P='1' then 
                     State <= D; 
-                END IF; 
-            WHEN others =>
+                end if; 
+            when others =>
                 State <= PADDING;
-        END CASE; 
-    END IF; 
-  END PROCESS;
-
-
-END rtl;
+        end case; 
+    end if; 
+  end process;
+end rtl;
